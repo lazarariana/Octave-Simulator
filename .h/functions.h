797f2265e950213347578a04885c632d1c79ba37
lazarariana <118962003+lazarariana@users.noreply.github.com>
2023-03-29 00:38:10 +0300

    Create functions.h

---
+++ .h/functions.h
@@ -0,0 +1,8 @@
+//Copyright Ariana-Maria Lazar-Andrei 312CAb 2022-2023
+#define R 10007
+
+int verif_index(int index1, int index2, int nr_matrix);
+void power(int ***a, int **b, int index, int *lin, int *col);
+void free_mat(int ***a, int nr_lin);
+int **create_mat(int nr_lin, int nr_col);
+void create_v(int **v, int lenght);
