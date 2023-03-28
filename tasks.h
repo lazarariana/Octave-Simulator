05cce3d286d349dcc6d66780c0ec8a1552eae8d6
lazarariana <118962003+lazarariana@users.noreply.github.com>
2023-03-29 00:38:39 +0300

    Add files via upload

---
+++ tasks.h
@@ -0,0 +1,13 @@
+//Copyright Ariana-Maria Lazar-Andrei 312CAb 2022-2023
+#define R 10007
+
+void task_L(int ****a, int *nr_matrix, int **lin, int **col);
+void task_D(int *lin, int *col, int nr_matrix);
+void task_P(int ***a, int *lin, int *col, int nr_matrix);
+void task_C(int ****a, int **lin, int **col, int nr_matrix);
+void task_M(int ****a, int *nr_matrix, int **lin, int **col);
+void task_O(int ***a, int *lin, int *col, int nr_matrix);
+void task_T(int ****a, int nr_matrix, int **lin, int **col);
+void task_R(int ****a, int *lin, int *col, int nr_matrix);
+void task_F(int ****a, int *nr_matrix, int **lin, int **col);
+void task_Q(int ****a, int **lin, int **col, int nr_matrix);
